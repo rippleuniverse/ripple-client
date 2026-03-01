ee2e330cadc2abd57caa3b7023d0060905214729
mfonisoebong <mfonisoischris@gmail.com>
2026-03-01 23:11:51 +0100

    fixes

--- components/common/footer/footer-info.tsx
+++ components/common/footer/footer-info.tsx
@@ -15,21 +15,55 @@ export const FooterInfo = () => {
         tech.
       </p>
       <div className="flex gap-3">
-        <Link href={"/"} className={"text-white"}>
+        <Link
+          target="_blank"
+          href={"https://www.facebook.com/share/17EtUn71jv/?mibextid=wwXIfr"}
+          className={"text-white"}
+        >
           <Facebook className={"size-6"} />
         </Link>
-        <Link href={"/"} className={"text-white"}>
+        <Link
+          target="_blank"
+          href={
+            " https://www.instagram.com/ripple.universe?igsh=MW94YnZjenZyYnFlZA=="
+          }
+          className={"text-white"}
+        >
           <Instagram className={"size-6"} />
         </Link>
-        <Link href={"/"} className={"text-white"}>
+        <Link
+          href={"https://x.com/ripple_universe?s=21"}
+          target="_blank"
+          className={"text-white"}
+        >
           <X size={22} />
         </Link>
-        <Link href={"/"} className={"text-white"}>
+        <Link
+          target="_blank"
+          href={"https://www.linkedin.com/company/ripple-universe"}
+          className={"text-white"}
+        >
           <Linkedin className={"size-6"} />
         </Link>
-        <Link href={"/"} className={"text-white"}>
+        <Link
+          target="_blank"
+          href={"https://youtube.com/@rippleuniversestudio?si=L8pSkL3i2Ld2haLi"}
+          className={"text-white"}
+        >
           <Youtube className={"size-6"} />
         </Link>
+      </div>
+      <div>
+        <p className="text-white text-xs">
+          <Link
+            href={
+              "https://www.instagram.com/ripple.pods?igsh=MWZkZWs2YjAwN204dA=="
+            }
+            target="_blank"
+          >
+            Ripple podcast
+          </Link>
+        </p>
       </div>
     </div>
   );
